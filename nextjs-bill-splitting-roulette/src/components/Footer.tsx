2ad07945d5529d6bb40a4824ed3cc55c0ed50379
nishipippi <sosuke@akane.waseda.jp>
2025-06-10 18:30:32 +0900

    ESLint2

--- nextjs-bill-splitting-roulette/src/components/Footer.tsx
+++ nextjs-bill-splitting-roulette/src/components/Footer.tsx
@@ -5,6 +5,7 @@ export const Footer: React.FC = () => {
   return (
     <footer className="mt-12 text-center text-sm text-white/80 pb-4 z-10">
       <p>&copy; {new Date().getFullYear()} nishipippi All Rights Reserved.</p>
+      {/* eslint-disable-next-line react/no-unescaped-entities */}
       <p>Inspired by nishipippi's portfolio design.</p>
     </footer>
   );
